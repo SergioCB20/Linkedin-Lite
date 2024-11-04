4cb198eb52c92c63595e988fe486f990561135ee
Sergio <sergiochumbi98@gmail.com>
2024-11-04 00:05:13 -0500

    version_1.1

--- client/src/components/Extra/Navbar.tsx
+++ client/src/components/Extra/Navbar.tsx
@@ -1,6 +1,11 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import {
+  faSearch,
+  faBell,
+  faBriefcase,
+  faHouse,
+} from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 import React from "react";
 
@@ -12,7 +17,7 @@ interface DropdownItemLinkProps {
 
 export default function Navbar() {
   return (
-    <nav className="w-full h-[60px] px-[10vw] bg-white">
+    <nav className="w-full h-[60px] px-[10vw] fixed bg-white">
       <ul className="w-full h-full flex flex-row justify-between items-center">
         <LeftSideNavBar />
         <RightSideNavBar />
@@ -23,13 +28,18 @@ export default function Navbar() {
 
 function LeftSideNavBar() {
   return (
-    <li className="flex flex-row gap-2 items-center">
-      <FontAwesomeIcon icon={faLinkedin} className="text-blue-600 text-4xl" />
+    <li className="flex flex-row gap-5 items-center w-1/2">
+      <Link to="/">
+        <FontAwesomeIcon
+          icon={faLinkedin}
+          className="text-blue-600 text-4xl hover:text-blue-700"
+        />
+      </Link>
       <label
-        className="input input-bordered w-[250px] h-[40px]
+        className="input input-bordered w-[450px] h-[40px]
             bg-slate-200 flex items-center text-black gap-2"
       >
-        <FontAwesomeIcon icon={faSearch} className="" />
+        <FontAwesomeIcon icon={faSearch} />
         <input type="text" className="" placeholder="Search" />
       </label>
     </li>
@@ -38,7 +48,25 @@ function LeftSideNavBar() {
 
 function RightSideNavBar() {
   return (
-    <li>
+    <li className="flex flex-row  items-center justify-around w-1/2">
+      <div className="flex flex-row gap-3 items-center ">
+        <FontAwesomeIcon icon={faHouse} className="text-3xl" />
+        <p>Home</p>
+      </div>
+      <div className="flex flex-row gap-3 items-center ">
+        <FontAwesomeIcon icon={faBriefcase} className="text-3xl" />
+        <p>Job Requests</p>
+      </div>
+      <div className="flex flex-row gap-3 items-center">
+        <div className="relative">
+          <div className="bg-red-600 text-xs text-center font-bold w-[15px] h-[15px]
+           text-white rounded-full absolute right-0">
+            2
+          </div>
+          <FontAwesomeIcon icon={faBell} className="text-3xl" />
+        </div>
+        <p>Notifications</p>
+      </div>
       <details className="dropdown dropdown-end">
         <summary className="avatar flex flex-col items-center pt-[5px]">
           <div className="w-10 rounded-full cursor-pointer">
@@ -49,11 +77,6 @@ function RightSideNavBar() {
           <DropdownItemLink
             className="btn bg-white border-2 border-blue-600 hover:bg-neutral-100 hover:border-blue-400 hover:text-black"
             title="View Profile"
-            link="/profile"
-          />
-          <DropdownItemLink
-            className="btn bg-white border-2 border-blue-600 hover:bg-neutral-100 hover:border-blue-400 hover:text-black"
-            title="Job Posting Account"
             link="/profile"
           />
           <DropdownItemLink
